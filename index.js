e09db3089a51b67aa9c176b828c1825ae66ec031
KonradPietocha <konrad.pietocha@gazeta.pl>
2020-08-03 18:51:51 +0200

    mk router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const wiki = require('./wiki.js');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/wiki', wiki);
 
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!')
